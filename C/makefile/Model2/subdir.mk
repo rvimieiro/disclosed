8e2854dd05cd2b2cf578debed51ad66bd877dff2
Renato Vimieiro <renato.vimieiro@newcastle.edu.au>
2014-02-18 11:20:08 +1100

    Cleaned up unused files and reorganized output dirs (renamed Release as Model2) to reflect structure described in the paper.

--- C/makefile/Model2/subdir.mk
+++ C/makefile/Model2/subdir.mk
@@ -29,7 +29,7 @@ C_DEPS += \
 %.o: ../%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -O3 -Wall -c -fmessage-length=0 -msse4.2 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -DModel2 -O3 -Wall -c -fmessage-length=0 -msse4.2 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
